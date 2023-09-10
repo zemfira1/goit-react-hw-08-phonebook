6da199be30fbc35b6ef570de322c04f178588708
zemfira1 <122248158+zemfira1@users.noreply.github.com>
2023-09-10 18:24:04 +0000

    Deploying to gh-pages from @ zemfira1/goit-react-hw-08-phonebook@d3adfb9875298f6c221ba8cc1ed2203a7010c966 🚀

---
+++ static/js/328.d07a11ca.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[328],{3328:function(n,t,e){e.r(t),e.d(t,{default:function(){return u}});var i,o=e(168),r=e(6088),a=e(6124),s=r.Z.div(i||(i=(0,o.Z)(["\n  font-size: ",";\n  width: ",";\n  margin-inline: auto;\n  margin-top: ",";\n  text-align: center;\n"])),a.r.fontSizes.xl,a.r.spacing(300),a.r.spacing(7)),c=e(184),u=function(){return(0,c.jsx)(s,{children:"Nothing found at this address."})}}}]);
+//# sourceMappingURL=328.d07a11ca.chunk.js.map
